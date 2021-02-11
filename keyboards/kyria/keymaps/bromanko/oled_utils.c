813c371f52c77adfe6c83d0c99c9b2ea0f0610a7
Brian Romanko <hello@bromanko.com>
2021-02-11 16:45:36 -0600

    Switch to invert for modifier indication

--- keyboards/kyria/keymaps/bromanko/oled_utils.c
+++ keyboards/kyria/keymaps/bromanko/oled_utils.c
@@ -1,11 +1,25 @@
 #include "oled_utils.h"
 #include <stdio.h>
+#include <string.h>
 
 char wpm_str[10];
+char hue_str[12];
+char sat_str[12];
+char bri_str[12];
 
 void render_wpm(void) {
     sprintf(wpm_str, "       WPM: %03d", get_current_wpm());
     oled_write(wpm_str, false);
+}
+
+void progress_str(char *meter, uint8_t val, uint8_t max) {
+    const uint8_t segments = 10;
+//    ratio = val / max;
+    strcat(meter, "[");
+    for (uint8_t i = 1; i < segments; i++) {
+        strcat(meter, " ");
+    }
+    strcat(meter, "]");
 }
 
 void render_status(void) {
@@ -40,37 +54,47 @@ void render_status(void) {
 
     uint8_t led_usb_state = host_keyboard_leds();
     oled_write_P(PSTR("Lock:    "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK)   ? PSTR("abc") : PSTR("   "), false);
+    oled_write_P(PSTR("abc"), IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK));
     oled_write_P(PSTR(" "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK)    ? PSTR("123") : PSTR("   "), false);
+    oled_write_P(PSTR("123"), IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK));
     oled_write_P(PSTR("\n"), false);
 
     uint16_t lsft = keyboard_report->mods & MOD_BIT(KC_LSHIFT);
     uint16_t rsft = keyboard_report->mods & MOD_BIT(KC_RSHIFT);
     oled_write_P(PSTR("Mods:    "), false);
-    oled_write_P((lsft || rsft) ? PSTR("S") : PSTR(" "), false);
+    oled_write_P(PSTR("S"), (lsft || rsft));
     oled_write_P(PSTR(" "), false);
 
     uint16_t lctr = keyboard_report->mods & MOD_BIT(KC_LCTRL);
     uint16_t rctr = keyboard_report->mods & MOD_BIT(KC_RCTRL);
-    oled_write_P((lctr || rctr) ? PSTR("C") : PSTR(" "), false);
+    oled_write_P(PSTR("C"), (lctr || rctr));
+    oled_write_P(PSTR(" "), false);
 
     uint16_t lalt = keyboard_report->mods & MOD_BIT(KC_LALT);
     uint16_t ralt = keyboard_report->mods & MOD_BIT(KC_RALT);
-    oled_write_P((lalt || ralt) ? PSTR("A") : PSTR(" "), false);
+    oled_write_P(PSTR("A"), (lalt || ralt));
     oled_write_P(PSTR(" "), false);
 
     uint16_t lgui = keyboard_report->mods & MOD_BIT(KC_LGUI);
     uint16_t rgui = keyboard_report->mods & MOD_BIT(KC_RGUI);
-    oled_write_P((lgui || rgui) ? PSTR("G") : PSTR(" "), false);
+    oled_write_P(PSTR("G"), (lgui || rgui));
     oled_write_P(PSTR("\n\n"), false);
 
     // RGB information
     if (rgblight_is_enabled()) {
-        oled_write_P(PSTR("Hue:     [----------]"), false);
-        oled_write_P(PSTR("Sat:     [----------]"), false);
-        oled_write_P(PSTR("Bri:     [----------]"), false);
+        oled_write_P(PSTR("Hue:     "), false);
+//        progress_str(hue_str, rgblight_get_hue(), 255);
+//        oled_write(hue_str, false);
+
+        oled_write_P(PSTR("Sat:     "), false);
+//        progress_str(sat_str, rgblight_get_sat(), 255);
+//        oled_write(sat_str, false);
+
+        oled_write_P(PSTR("Bri:     "), false);
+//        progress_str(bri_str, rgblight_get_val(), RGBLIGHT_LIMIT_VAL);
+//        oled_write_P(PSTR(bri_str), false);
     } else {
         oled_write_P(PSTR("RGB:     Off\n\n\n"), false);
     }
 }
+

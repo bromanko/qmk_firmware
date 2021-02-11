31252a384f0839823212feb99a5e22dfcb16cd4e
Brian Romanko <hello@bromanko.com>
2021-02-11 08:41:59 -0600

    Add new OLED layout
    Wire up the layer and lock displays.

--- keyboards/kyria/keymaps/bromanko/oled_utils.c
+++ keyboards/kyria/keymaps/bromanko/oled_utils.c
@@ -1,15 +1,6 @@
 #include "oled_utils.h"
 #include <stdio.h>
 
-static void render_qmk_logo(void) {
-  static const char PROGMEM qmk_logo[] = {
-    0x80,0x81,0x82,0x83,0x84,0x85,0x86,0x87,0x88,0x89,0x8a,0x8b,0x8c,0x8d,0x8e,0x8f,0x90,0x91,0x92,0x93,0x94,
-    0xa0,0xa1,0xa2,0xa3,0xa4,0xa5,0xa6,0xa7,0xa8,0xa9,0xaa,0xab,0xac,0xad,0xae,0xaf,0xb0,0xb1,0xb2,0xb3,0xb4,
-    0xc0,0xc1,0xc2,0xc3,0xc4,0xc5,0xc6,0xc7,0xc8,0xc9,0xca,0xcb,0xcc,0xcd,0xce,0xcf,0xd0,0xd1,0xd2,0xd3,0xd4,0};
-
-  oled_write_P(qmk_logo, false);
-}
-
 char wpm_str[10];
 
 void render_wpm(void) {
@@ -18,41 +9,49 @@ void render_wpm(void) {
 }
 
 void render_status(void) {
-    // QMK Logo and version information
-    render_qmk_logo();
-    oled_write_P(PSTR("       Kyria rev1.0\n\n"), false);
-
-    // Host Keyboard Layer Status
-    oled_write_P(PSTR("Layer: "), false);
+    // Status
+    oled_write_P(PSTR("Layer:   "), false);
     switch (get_highest_layer(layer_state)) {
         case BASE:
-            oled_write_P(PSTR("Base\n"), false);
+            oled_write_P(PSTR("Base"), false);
             break;
         case MOUR:
-            oled_write_P(PSTR("Mouse\n"), false);
+            oled_write_P(PSTR("Mouse"), false);
             break;
         case NAVR:
-            oled_write_P(PSTR("Navigation\n"), false);
+            oled_write_P(PSTR("Navigation"), false);
             break;
         case MEDR:
-            oled_write_P(PSTR("Media\n"), false);
+            oled_write_P(PSTR("Media"), false);
             break;
         case NSSL:
-            oled_write_P(PSTR("Symbols\n"), false);
+            oled_write_P(PSTR("Symbols"), false);
             break;
         case NSL:
-            oled_write_P(PSTR("Numbers\n"), false);
+            oled_write_P(PSTR("Numbers"), false);
             break;
         case FUNL:
-            oled_write_P(PSTR("Functions\n"), false);
+            oled_write_P(PSTR("Functions"), false);
             break;
         default:
-            oled_write_P(PSTR("Undefined\n"), false);
+            oled_write_P(PSTR("???"), false);
     }
+    oled_write_P(PSTR("\n"), false);
+
+    uint8_t led_usb_state = host_keyboard_leds();
+    oled_write_P(PSTR("Lock:    "), false);
+    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK)   ? PSTR("abc") : PSTR("   "), false);
+    oled_write_P(PSTR(" "), false);
+    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK)    ? PSTR("123") : PSTR("   "), false);
+    oled_write_P(PSTR("\n"), false);
+
+    oled_write_P(PSTR("Mods:    S C A G"), false);
+    oled_write_P(PSTR("\n\n"), false);
+
+    // RGB information
+    oled_write_P(PSTR("Hue:     [----------]"), false);
+    oled_write_P(PSTR("Sat:     [----------]"), false);
+    oled_write_P(PSTR("Bri:     [----------]"), false);
 
     // Host Keyboard LED Status
-    uint8_t led_usb_state = host_keyboard_leds();
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_NUM_LOCK)    ? PSTR("NUMLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_CAPS_LOCK)   ? PSTR("CAPLCK ") : PSTR("       "), false);
-    oled_write_P(IS_LED_ON(led_usb_state, USB_LED_SCROLL_LOCK) ? PSTR("SCRLCK ") : PSTR("       "), false);
 }

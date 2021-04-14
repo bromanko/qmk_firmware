9b4f6d5b3c1e2380aeacb68f66c5d10a67504592
Brian Romanko <hello@bromanko.com>
2021-04-14 08:32:58 -0500

    Updating timings for Torn

--- keyboards/torn/keymaps/bromanko/keymap.c
+++ keyboards/torn/keymaps/bromanko/keymap.c
@@ -1,7 +1,10 @@
 #include QMK_KEYBOARD_H
 #include "manna-harbour_miryoku.h"
+#include "raw_hid.h"
+#define RAW_EPSIZE 8
 
 bool led_update_user(led_t led_state) {
     torn_set_led(0, led_state.caps_lock);
     return true;
 }
+
